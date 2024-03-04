f15ae4cbd8ff0099147d3e7799eccc730c5651be
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-03-04 23:23:26 +0530

    Verification http functions implemented
    Commit contains
    welcome page http function
    send otp http function
    verify otp http function

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,24 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
+const admin = require("firebase-admin");
+const serviceAccount = require("./service_account_key.json");
+const { onRequest } = require("firebase-functions/v2/https");
+const cors = require("cors");
 const logger = require("firebase-functions/logger");
+const { setGlobalOptions } = require("firebase-functions/v2");
+const express = require("express");
+const app = express();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  projectId: "android-now-14",
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//http functions
+const welcome = require("./main/https/landing/welcome");
+app.use("/", welcome);
+const sendOtpToPhoneNumber = require("./main/https/verification/send_otp_to_phone_number");
+app.use("/verification/phone-number/send-otp", sendOtpToPhoneNumber);
+const verifyPhoneNumberOtp = require("./main/https/verification/verify_phone_number_otp");
+app.use("/verification/phone-number/verify-otp", verifyPhoneNumberOtp);
+
+setGlobalOptions({ maxInstances: 10 });
+exports.androidnow = onRequest(app);
