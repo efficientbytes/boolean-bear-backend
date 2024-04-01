e193c78e1bd56ac38f2960c22ee21b71962cdc65
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-04-02 01:19:49 +0530

    Introduced shuffled and curated categories.

--- functions/main/https/content-categories/create_content_categories.js
+++ functions/main/https/content-categories/create_content_categories.js
@@ -22,51 +22,107 @@ class Category {
 router.get("/", async (request, response) => {
     logger.log(`http||create-content-categories.`);
 
+    const type = request.query.type || "shuffled";
+    logger.log(`log|| type is ${type}`);
+
     const responseBody = {
         message: null,
     };
 
-    const categoriesData = [
-        new Category("Kotlin", 1),
-        new Category("Git", 2),
-        new Category("Podcasts", 9),
-        new Category("Kotlin multiplatform", 7),
-        new Category("Figma", 3),
-        new Category("Android XML", 6),
-        new Category("Android Compose", 4),
-        new Category("Backend Development", 5),
-        new Category("Desktop Development", 8)
-    ];
-
-    const categoriesPath = `/ASSETS/CATEGORIES/FILES/`;
-    const categoryRef = admin.firestore().collection(categoriesPath);
-    const time = admin.firestore.FieldValue.serverTimestamp();
-    try {
-        for (const category of categoriesData) {
-            const categoryId = categoryRef.doc().id;
-            category.id = categoryId;
-            await categoryRef.doc(categoryId).set({
-                id: category.id,
-                index: category.index,
-                title: category.title,
-                caption: "",
-                contentCount: 2,
-                deepLink: "",
-                type1Thumbnail: "",
-                contentIds: ["", ""],
-                searchTags: ["", ""],
-                dateCreated: time,
-                dateModified: time
-            })
-            ;
+    if (type === "shuffled") {
+        const categoriesData = [
+            new Category("Kotlin", 1),
+            new Category("Git", 2),
+            new Category("Podcasts", 9),
+            new Category("Kotlin multiplatform", 7),
+            new Category("Figma", 3),
+            new Category("Android XML", 6),
+            new Category("Android Compose", 4),
+            new Category("Backend Development", 5),
+            new Category("Desktop Development", 8)
+        ];
+        const categoriesPath = `/ASSETS/CATEGORIES/SHUFFLED/`;
+        const categoryRef = admin.firestore().collection(categoriesPath);
+        const time = admin.firestore.FieldValue.serverTimestamp();
+        try {
+            for (const category of categoriesData) {
+                const categoryId = categoryRef.doc().id;
+                category.id = categoryId;
+                await categoryRef.doc(categoryId).set({
+                    id: category.id,
+                    index: category.index,
+                    title: category.title,
+                    caption: "",
+                    contentCount: 2,
+                    deepLink: "",
+                    type1Thumbnail: "",
+                    contentIds: ["", ""],
+                    searchTags: ["", ""],
+                    dateCreated: time,
+                    dateModified: time,
+                    categoryType: "SHUFFLED"
+                })
+                ;
+            }
+            logger.log(`log||Shuffled categories have been successfully uploaded.`);
+            responseBody.message = `Shuffled categories have been successfully uploaded.`;
+            response.status(200).send(responseBody);
+        } catch (error) {
+            logger.error(`log||error uploading categories. Error is ${error.message}`);
+            responseBody.message = `Could not upload the shuffled categories, Error is ${error.message}`;
+            response.status(500).send(responseBody);
         }
-        logger.log(`log|| Categories have been successfully uploaded.`);
-        responseBody.message = `Categories have been successfully uploaded.`;
-        response.status(200).send(responseBody);
-    } catch (error) {
-        logger.error(`log|| Error uploading categories.`);
-        responseBody.message = `Could not upload the categories, Error is ${error.message}`;
-        response.status(500).send(responseBody);
+
+        return;
+    }
+
+    if (type === "curated") {
+
+        const categoriesData = [
+            new Category("Kotlin", 1),
+            new Category("Git", 2),
+            new Category("Podcasts", 9),
+            new Category("Kotlin multiplatform", 7),
+            new Category("Figma", 3),
+            new Category("Android XML", 6),
+            new Category("Android Compose", 4),
+            new Category("Backend Development", 5),
+            new Category("Desktop Development", 8)
+        ];
+        const categoriesPath = `/ASSETS/CATEGORIES/CURATED/`;
+        const categoryRef = admin.firestore().collection(categoriesPath);
+
+        const time = admin.firestore.FieldValue.serverTimestamp();
+        try {
+            for (const category of categoriesData) {
+                const categoryId = categoryRef.doc().id;
+                category.id = categoryId;
+                await categoryRef.doc(categoryId).set({
+                    id: category.id,
+                    index: category.index,
+                    title: category.title,
+                    caption: "",
+                    contentCount: 2,
+                    deepLink: "",
+                    type1Thumbnail: "",
+                    curatedContentIds: ["", ""],
+                    searchTags: ["", ""],
+                    dateCreated: time,
+                    dateModified: time,
+                    categoryType: "CURATED"
+                })
+                ;
+            }
+            logger.log(`log||curated categories have been successfully uploaded.`);
+            responseBody.message = `Curated categories have been successfully uploaded.`;
+            response.status(200).send(responseBody);
+        } catch (error) {
+            logger.error(`log||error uploading categories. Error is ${error.message}`);
+            responseBody.message = `Could not upload the curated categories, Error is ${error.message}`;
+            response.status(500).send(responseBody);
+        }
+
+        return;
     }
 
 });
--- functions/main/https/content-categories/get_content_categories.js
+++ functions/main/https/content-categories/get_content_categories.js
@@ -2,62 +2,127 @@ const admin = require("firebase-admin");
 const logger = require("firebase-functions/logger");
 const express = require("express");
 const router = express.Router();
-router.get("/", async (request, response) => {
+router.get("/:type", async (request, response) => {
     logger.log(`http||get-content-categories.`);
+
+
+    const type = request.params.type || "shuffled";
 
     const responseBody = {
         message: null,
         categoryList: null
     };
 
-    const categoriesPath = `/ASSETS/CATEGORIES/FILES/`;
-    const categoryRef = admin.firestore().collection(categoriesPath);
+    if (type === "shuffled") {
 
-    try {
+        const categoriesPath = `/ASSETS/CATEGORIES/SHUFFLED/`;
+        const categoryRef = admin.firestore().collection(categoriesPath);
 
-        const categoriesResult = await categoryRef.get();
+        try {
 
-        if (categoriesResult.empty) {
-            logger.error(`log||There are no categories`);
+            const categoriesResult = await categoryRef.get();
 
-            responseBody.message = `There are no categories`;
-            responseBody.categoryList = null;
-            response.status(404).send(responseBody);
-            return;
-        }
+            if (categoriesResult.empty) {
+                logger.error(`log||There are no categories`);
 
-        const categoriesList = []
-        for (const categorySnapshot of categoriesResult.docs) {
-
-            const category = categorySnapshot.data();
-
-            const contentCategory = {
-                id: category.id,
-                index: category.index,
-                title: category.title,
-                caption: category.caption,
-                contentCount: category.contentCount,
-                deepLink: category.deepLink,
-                type1Thumbnail: category.type1Thumbnail,
-                contentIds: category.contentIds,
-                searchTags: category.searchTags,
-                dateCreated: category.dateCreated._seconds,
-                dateModified: category.dateModified._seconds
+                responseBody.message = `There are no categories`;
+                responseBody.categoryList = null;
+                response.status(404).send(responseBody);
+                return;
             }
 
-            categoriesList.push(contentCategory);
+            const categoriesList = []
+            for (const categorySnapshot of categoriesResult.docs) {
+
+                const category = categorySnapshot.data();
+
+                const contentCategory = {
+                    id: category.id,
+                    index: category.index,
+                    title: category.title,
+                    caption: category.caption,
+                    contentCount: category.contentCount,
+                    deepLink: category.deepLink,
+                    type1Thumbnail: category.type1Thumbnail,
+                    contentIds: category.contentIds,
+                    searchTags: category.searchTags,
+                    dateCreated: category.dateCreated._seconds,
+                    dateModified: category.dateModified._seconds,
+                    categoryType: "SHUFFLED"
+                }
+
+                categoriesList.push(contentCategory);
+            }
+
+            responseBody.message = `Successfully fetched all content categories`;
+            responseBody.categoryList = categoriesList;
+            response.status(200).send(responseBody);
+
+        } catch (error) {
+            logger.error(`log||Error fetching content categories.`);
+
+            responseBody.message = error.message;
+            responseBody.categoryList = null;
+            response.status(500).send(responseBody);
         }
 
-        responseBody.message = `Successfully fetched all content categories`;
-        responseBody.categoryList = categoriesList;
-        response.status(200).send(responseBody);
+        return;
+    }
 
-    } catch (error) {
-        logger.error(`log||Error fetching content categories.`);
+    if (type === "curated") {
 
-        responseBody.message = error.message;
-        responseBody.categoryList = null;
-        response.status(500).send(responseBody);
+        const categoriesPath = `/ASSETS/CATEGORIES/CURATED/`;
+        const categoryRef = admin.firestore().collection(categoriesPath);
+
+        try {
+
+            const categoriesResult = await categoryRef.get();
+
+            if (categoriesResult.empty) {
+                logger.error(`log||There are no categories`);
+
+                responseBody.message = `There are no categories`;
+                responseBody.categoryList = null;
+                response.status(404).send(responseBody);
+                return;
+            }
+
+            const categoriesList = []
+            for (const categorySnapshot of categoriesResult.docs) {
+
+                const category = categorySnapshot.data();
+
+                const contentCategory = {
+                    id: category.id,
+                    index: category.index,
+                    title: category.title,
+                    caption: category.caption,
+                    contentCount: category.contentCount,
+                    deepLink: category.deepLink,
+                    type1Thumbnail: category.type1Thumbnail,
+                    curatedContentIds: category.curatedContentIds,
+                    searchTags: category.searchTags,
+                    dateCreated: category.dateCreated._seconds,
+                    dateModified: category.dateModified._seconds,
+                    categoryType: "CURATED"
+                }
+
+                categoriesList.push(contentCategory);
+            }
+
+            responseBody.message = `Successfully fetched all content categories`;
+            responseBody.categoryList = categoriesList;
+            response.status(200).send(responseBody);
+
+        } catch (error) {
+            logger.error(`log||Error fetching content categories.`);
+
+            responseBody.message = error.message;
+            responseBody.categoryList = null;
+            response.status(500).send(responseBody);
+        }
+
+        return;
     }
 
 });
