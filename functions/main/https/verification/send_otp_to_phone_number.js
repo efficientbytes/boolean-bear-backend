f1ff429990b21f87f070b11dc4ecc80c29994033
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-04-20 23:04:14 +0530

    Verification folder

--- functions/main/https/verification/send_otp_to_phone_number.js
+++ functions/main/https/verification/send_otp_to_phone_number.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const logger = require("firebase-functions/logger");
 require("dotenv").config();
 const twilioServiceSid = process.env.TWILIO_SERVICE_SID;
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -8,44 +7,35 @@ const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 const twilio = require("twilio")(twilioAccountSid, twilioAuthToken);
 
 router.post("/", async (request, response) => {
-  logger.log(`http||send-otp-to-phone-number`);
+    const phoneNumber = request.body.phoneNumber || null;
 
-  const phoneNumber = request.body.phoneNumber || null;
-  logger.log(`param||phone number : ${phoneNumber}`);
-
-  if (phoneNumber == null) {
-    logger.log(`error||phone number is null`);
-
-    response.status(400).send({
-      message: `Phone number is not provided`,
-      phoneNumber: phoneNumber,
-    });
-    return;
-  }
-
-  twilio.verify.v2
-    .services(twilioServiceSid)
-    .verifications.create({ to: `+91${phoneNumber}`, channel: "sms" })
-    .then((verification) => {
-      logger.log(`log||OTP sent to phone number ${phoneNumber}`);
-
-      if (verification.status === "pending") {
-        response.status(200).send({
-          message: `OTP has been sent +91${phoneNumber}`,
-          phoneNumber: phoneNumber,
+    if (phoneNumber == null) {
+        response.status(400).send({
+            message: `Phone number is not provided`,
+            phoneNumber: phoneNumber,
         });
-      }
-    })
-    .catch((error) => {
-      logger.log(
-        `log||verification failed for phone number : ${phoneNumber}, error is: ${error.code}`,
-      );
+        return;
+    }
 
-      response.status(503).send({
-        message: `Verification error identifier ${error.code}`,
-        phoneNumber: phoneNumber,
-      });
-    });
+    twilio.verify.v2
+        .services(twilioServiceSid)
+        .verifications.create({to: `+91${phoneNumber}`, channel: "sms"})
+        .then((verification) => {
+
+            if (verification.status === "pending") {
+                response.status(200).send({
+                    message: `OTP has been sent +91${phoneNumber}`,
+                    phoneNumber: phoneNumber,
+                });
+            }
+        })
+        .catch((error) => {
+
+            response.status(503).send({
+                message: `Verification error identifier ${error.code}`,
+                phoneNumber: phoneNumber,
+            });
+        });
 });
 
 module.exports = router;
