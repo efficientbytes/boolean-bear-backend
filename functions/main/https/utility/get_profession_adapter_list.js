e3e88649e49a1168e059a6f9e38acaa71768412b
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-04-20 23:00:16 +0530

    Utility folder

--- functions/main/https/utility/get_profession_adapter_list.js
+++ functions/main/https/utility/get_profession_adapter_list.js
@@ -1,36 +1,30 @@
 const admin = require("firebase-admin");
-const logger = require("firebase-functions/logger");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", async (request, response) => {
-  logger.log(`http||get-profession-adapter-list.`);
+    const professionsAdapterListPath = `/UTILITY/APP/PROFESSIONS/`;
+    const professionAdapterListPathRef = admin
+        .firestore()
+        .collection(professionsAdapterListPath);
+    const professionAdapterListSnapshot =
+        await professionAdapterListPathRef.get();
+    const responseBody = [];
 
-  const professionsAdapterListPath = `/UTILITY/APP/PROFESSIONS/`;
-  const professionAdapterListPathRef = admin
-    .firestore()
-    .collection(professionsAdapterListPath);
-  const professionAdapterListSnapshot =
-    await professionAdapterListPathRef.get();
-  const responseBody = [];
+    if (professionAdapterListSnapshot.empty) {
+        response.status(500).send(responseBody);
+        return;
+    }
 
-  if (professionAdapterListSnapshot.empty) {
-    //profession data not available
-
-    logger.error(`log||profession adapter list data is empty in database.`);
-    response.status(500).send(responseBody);
-    return;
-  }
-
-  await professionAdapterListSnapshot.forEach((snapshot) => {
-    const professionData = snapshot.data();
-    responseBody.push({
-      index: professionData.index,
-      name: professionData.name,
+    await professionAdapterListSnapshot.forEach((snapshot) => {
+        const professionData = snapshot.data();
+        responseBody.push({
+            index: professionData.index,
+            name: professionData.name,
+        });
     });
-  });
 
-  response.status(200).send(responseBody);
+    response.status(200).send(responseBody);
 });
 
 module.exports = router;
