5049c3e4858cc04a18a688133d5d8c9431186f6a
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-05-23 18:37:45 +0530

    Password creation

---
+++ functions/main/https/authentication/password_authentication.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const router = express.Router();
+const admin = require("firebase-admin");
+const {logger} = require("firebase-functions");
+const bcrypt = require("bcrypt");
+
+function validatePassword(password) {
+    if (!password) {
+        return "Password cannot be empty.";
+    }
+    if (password.length < 12) {
+        return "Password must be at least 12 characters long.";
+    }
+    if (!/[A-Z]/.test(password)) {
+        return "Password must contain at least one uppercase letter.";
+    }
+    if (!/[a-z]/.test(password)) {
+        return "Password must contain at least one lowercase letter.";
+    }
+    if (!/[0-9]/.test(password)) {
+        return "Password must contain at least one number.";
+    }
+    if (!/[$#@_!]/.test(password)) {
+        return "Password must contain at least one special character: $#@_!";
+    }
+    return null;
+}
+
+
+router.post("/", async (request, response) => {
+
+    const password = request.body.password || null;
+    const userAccountId = request.body.userAccountId || null;
+
+    const responseBody = {
+        data: null,
+        message: null,
+    };
+
+    responseBody.data = {
+        phoneNumber: null,
+        prefix: null,
+    }
+
+    if (password == null) {
+        responseBody.message = `Password is not provided`;
+        response.status(400).send(responseBody);
+        return;
+    }
+
+    if (userAccountId == null) {
+        responseBody.message = `User account id is not provided`;
+        response.status(400).send(responseBody);
+        return;
+    }
+
+    const validationResponse = validatePassword(password);
+
+    if (validationResponse != null) {
+        responseBody.message = validationResponse;
+        response.status(400).send(responseBody);
+        return
+    }
+
+    //hash the password
+    const passwordPath = `/USER/PASSWORDS/FILES/${userAccountId}`;
+    const passwordRef = admin.firestore().doc(passwordPath);
+    const passwordQueryResult = await passwordRef.get();
+
+    if (!passwordQueryResult.exists) {
+        responseBody.message = `User does not exist in the server.`;
+        response.status(400).send(responseBody);
+        return
+    }
+
+    const passwordData = passwordQueryResult.data();
+    const serverHash = passwordData.hash;
+
+    try {
+        const match = await bcrypt.compare(password, serverHash);
+        if (match) {
+            // Password matches
+
+            const userProfilePath = `/USER/PRIVATE-PROFILE/FILES/${userAccountId}`;
+            const userProfileQueryRef = admin.firestore().doc(userProfilePath);
+            const userProfileQueryResult = await userProfileQueryRef.get();
+
+            if (!userProfileQueryResult.exists) {
+                responseBody.message = `Account does not exists.`;
+                response.status(400).send(responseBody);
+                return
+            }
+
+            const userProfileData = userProfileQueryResult.data();
+            const phoneNumber = userProfileData.phoneNumber;
+            const phoneNumberPrefix = userProfileData.phoneNumberPrefix;
+
+            responseBody.data.phoneNumber = phoneNumber;
+            responseBody.data.prefix = phoneNumberPrefix;
+            responseBody.message = `Authentication successful.`;
+            response.status(200).send(responseBody);
+
+        } else {
+            // Password does not match
+
+            responseBody.message = `Incorrect credential.`;
+            response.status(400).send(responseBody);
+        }
+    } catch (err) {
+        responseBody.message = `Failed to process the request.`;
+        response.status(500).send(responseBody);
+    }
+
+});
+
+module.exports = router;
---
+++ functions/main/https/authentication/update_account_password.js
@@ -0,0 +1,131 @@
+const express = require("express");
+const router = express.Router();
+const admin = require("firebase-admin");
+const bcrypt = require("bcrypt");
+
+function validatePassword(password) {
+    if (!password) {
+        return "Password cannot be empty.";
+    }
+    if (password.length < 12) {
+        return "Password must be at least 12 characters long.";
+    }
+    if (!/[A-Z]/.test(password)) {
+        return "Password must contain at least one uppercase letter.";
+    }
+    if (!/[a-z]/.test(password)) {
+        return "Password must contain at least one lowercase letter.";
+    }
+    if (!/[0-9]/.test(password)) {
+        return "Password must contain at least one number.";
+    }
+    if (!/[$#@_!]/.test(password)) {
+        return "Password must contain at least one special character: $#@_!";
+    }
+    return null;
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+router.post("/", async (request, response) => {
+
+    if (
+        !request.headers.authorization ||
+        !request.headers.authorization.startsWith("Bearer ")
+    ) {
+        response.status(401).send({message: `Authentication required.`});
+        return;
+    }
+    const idToken = request.headers.authorization.split(' ')[1];
+    let userAccountId;
+    try {
+        const tokenData = await admin.auth().verifyIdToken(idToken);
+        if (tokenData == null) {
+            response.status(401).send({message: `Invalid auth token`});
+            return;
+        }
+        userAccountId = tokenData.uid;
+        if (userAccountId == null) {
+            response.status(401).send({message: `Invalid auth token`});
+            return;
+        }
+    } catch (error) {
+        response.status(401).send({message: `Invalid auth token`});
+        return;
+    }
+
+    const password = request.body.password || null;
+
+    const responseBody = {
+        message: null,
+    };
+
+    if (password == null) {
+        responseBody.message = `Password is not provided`;
+        response.status(400).send(responseBody);
+        return;
+    }
+
+    const validationResponse = validatePassword(password);
+
+    if (validationResponse != null) {
+        responseBody.message = validationResponse;
+        response.status(400).send(responseBody);
+        return
+    }
+
+    //hash the password
+    const passwordPath = `/USER/PASSWORDS/FILES/${userAccountId}`;
+    const passwordRef = admin.firestore().doc(passwordPath);
+    const passwordQueryResult = await passwordRef.get();
+
+    if (!passwordQueryResult.exists) {
+        responseBody.message = `Identity could not be verified.`;
+        response.status(400).send(responseBody);
+        return
+    }
+
+    const passwordData = passwordQueryResult.data();
+    const serverHash = passwordData.hash;
+
+    try {
+        const match = await bcrypt.compare(password, serverHash);
+        if (match) {
+            // New password cannot be same as the old password
+            responseBody.message = `New password cannot be same as the old password`;
+            response.status(400).send(responseBody);
+        } else {
+            // Proceed with hashing and save it in database
+            try {
+                const saltRounds = getRandomNumber(10, 18);
+                const salt = await bcrypt.genSalt(saltRounds);
+                const hashedPassword = await bcrypt.hash(password, salt);
+
+                const time = admin.firestore.FieldValue.serverTimestamp();
+                await passwordRef.update({
+                    hash: hashedPassword,
+                    updatedOn: time,
+                }).then(result => {
+                    responseBody.message = `Password has been successfully updated.`;
+                    response.status(200).send(responseBody);
+                }).catch(error => {
+                    responseBody.message = error.message;
+                    response.status(500).send(responseBody);
+                });
+
+            } catch (err) {
+                responseBody.message = `Failed to accept the password.`;
+                response.status(500).send(responseBody);
+            }
+
+        }
+    } catch (err) {
+        responseBody.message = `Failed to accept the password.`;
+        response.status(500).send(responseBody);
+    }
+
+});
+
+module.exports = router;
