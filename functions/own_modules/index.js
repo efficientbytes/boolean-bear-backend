c08838285c0ad8adf84122d0725393631345ff12
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-07-10 23:28:58 +0530

    implemented one time app check token

--- functions/own_modules/index.js
+++ functions/own_modules/index.js
@@ -1,2 +1,5 @@
 const verifyAppCheckToken = require("./verify_app_check_token.js");
-exports.verifyAppCheckToken = verifyAppCheckToken
+exports.verifyAppCheckToken = verifyAppCheckToken;
+
+const verifyOneTimeAppCheckToken = require("./verify_one_time_app_check_token.js");
+exports.verifyOneTimeAppCheckToken = verifyOneTimeAppCheckToken;
