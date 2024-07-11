3544b8374e0fb8e1f91f6dc43b0e0eff9ca77126
Anubhav-PS <anubhav.p.sahoo@gmail.com>
2024-07-11 12:13:48 +0530

    implemented id token verification in courses api

--- functions/main/https/courses/join_course_waiting_list.js
+++ functions/main/https/courses/join_course_waiting_list.js
@@ -2,34 +2,11 @@ const admin = require("firebase-admin");
 const express = require("express");
 const router = express.Router();
 const {verifyAppCheckToken} = require("own_modules/verify_app_check_token.js");
+const {verifyIdToken} = require("own_modules/verify_id_token.js");
 
-router.post("/:courseId/join-waiting-list", verifyAppCheckToken, async (request, response) => {
+router.post("/:courseId/join-waiting-list", verifyAppCheckToken, verifyIdToken, async (request, response) => {
 
-    if (
-        !request.headers.authorization ||
-        !request.headers.authorization.startsWith("Bearer ")
-    ) {
-        response.status(401).send({message: `Authentication required.`});
-        return;
-    }
-    const idToken = request.headers.authorization.split(' ')[1];
-    let userAccountId;
-    try {
-        const tokenData = await admin.auth().verifyIdToken(idToken);
-        if (tokenData == null) {
-            response.status(401).send({message: `Invalid auth token`});
-            return;
-        }
-        userAccountId = tokenData.uid;
-        if (userAccountId == null) {
-            response.status(401).send({message: `Invalid auth token`});
-            return;
-        }
-    } catch (error) {
-        response.status(401).send({message: `Invalid auth token`});
-        return;
-    }
-
+    const userAccountId = request.userAccountId;
     const courseId = request.params.courseId || null;
 
     const responseBody = {
